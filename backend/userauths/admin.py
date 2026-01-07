c6770934800bd290cae980c64e44a0d28b39c45c
Islam412 <islam.124.hamdy@gmail.com>
2026-01-08 01:51:58 +0200

    desing profile admin

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -12,3 +12,11 @@ class UserCustomAdmin(admin.ModelAdmin):
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     filter_horizontal = ('user_permissions',)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'email', 'username', 'address_info', 'phone_info', 'verified']  # إضافة 'full_name', 'email', 'username'
+    list_editable = ['verified']
+    search_fields = ['user__username', 'phone', 'address']
+    list_filter = ['verified']
+    ordering = ['user']
